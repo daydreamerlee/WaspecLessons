22fb3085595f47cbd3feea41ded01b3d7a31eec2
daydreamerlee <daydreamerlee@gmail.com>
2015-03-14 17:27:33 -0700

    First Uploaded up to 314

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-import org.waspec.lesson017.*;
-import org.waspec.lesson018.UseListExampel1;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("Test test");
 //        TestOfTeacher.test();
     }
 }
