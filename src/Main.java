e0612b13bc1f5412f4b3a4e9c111ec11ab44b416
daydreamerlee <daydreamerlee@gmail.com>
2015-03-14 18:40:33 -0700

    Second upload with testtest2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Test test");
+        System.out.println("Test test 2");
 //        TestOfTeacher.test();
     }
 }
